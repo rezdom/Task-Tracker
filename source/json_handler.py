ce074cb8099813afc6cc3c227aa3a096120707a6
rezdom <pavlik333333@mail.ru>
2024-11-18 20:57:39 +0300

    v0.30 added update of task

--- source/json_handler.py
+++ source/json_handler.py
@@ -3,6 +3,7 @@ from os import listdir
 from os.path import join, basename
 import sys
 from getpass import getuser
+from datetime import datetime
 
 from utils.task import Task
 from utils.app_exeptions import TaskLimitError
@@ -42,15 +43,62 @@ class TaskJsonHandler:
             data["src"]["task_id"] += 1
             file.seek(0)
             json.dump(data, file, ensure_ascii=False, indent=4)
+    
+    def update(self, task_id: int, task_description: str = None, task_title: str = None) -> None:
+        with open(self.file_name, "r+", encoding="utf-8") as file:
+            data = json.load(file)
+            if not isinstance(task_id, int):
+                raise TypeError(
+                 f"The object is of type [{type(task_id).__name__}], "
+                "but a [integer] object was expected."
+            )
+            task_id = str(task_id)
+
+            task = None
+            for status in data:
+                if task_id in data[status]:
+                    task = data[status][task_id]
+                    break
+            if task is None:
+                raise ValueError(f"The provided ID does not exist in the database.")
+            
+            task = Task(
+                task["title"],
+                task["description"],
+                datetime.strptime(task["createdAt"], "%d-%m-%Y, %H:%M:%S"),
+                datetime.strptime(task["updatedAt"], "%d-%m-%Y, %H:%M:%S"),
+                task["status"]
+            )
+
+            if task_description is None and task_title is None:
+                raise ValueError("Arguments were not provided.")
+            else:
+                if task_description is not None:
+                    task.description = task_description
+                if task_title is not None:
+                    task.title = task_title
+                task.updatedAt = datetime.now()
+            data[self.TASK_STATUS[task.status]][task_id] = self.__date_to_str(task.__dict__)
+            file.seek(0)
+            json.dump(data, file, ensure_ascii=False, indent=4)
+            #The file.truncate() clears remaining content in the file, 
+            #preventing leftover data from being appended after updating the JSON.
+            file.truncate() 
+
 
 if __name__ == "__main__":
     data = [int(item) if item.isdigit() else item for item in sys.argv]
     task_handler = TaskJsonHandler()
     commands = {
-        "add": (task_handler.add, 2)
+        "add": (task_handler.add,1, 2),
+        "update": (task_handler.update,2, 3),
+        "update_title": (task_handler.update,2, 2)
     }
     try:
-        if data[1] in commands and len(data) - 2 in range(1,commands[data[1]][1]+1):
-            commands[data[1]][0](*data[2:])
+        if data[1] in commands and len(data) - 2 in range(commands[data[1]][1],commands[data[1]][2]+1):
+            if data[1] == list(commands.keys())[-1] and len(data) - 2 in range(commands[data[1]][1],commands[data[1]][2]+1):
+                task_handler.update(data[2], task_title=data[3])
+            else:
+                commands[data[1]][0](*data[2:])
     except Exception as e:
         print(e)
